7d0b368440f1c43197847d8bce291f88ca74bbe6
Liang Hao <haoliang302@gmail.com>
2016-11-22 11:53:49 -0800

    minor code fixes

--- code/scripts/eda-script.R
+++ code/scripts/eda-script.R
@@ -1,3 +1,18 @@
-library(plyr)
-args = commandArgs(trailingOnly =TRUE)
+# This is the file for Exploratory Data Analysis 
+# of the dataset for the project
 
+#college = read.csv('data/combinedData.csv')
+
+library(plyr)
+library(maps)
+library(ggplot2)
+args = commandArgs(trailingOnly =TRUE)
+college = read.csv(args[1], header =T)
+college = college[,2:ncol(college)]
+
+# First the overall information
+dim(college)
+print('There are 6715 colleges in our dataset')
+
+# By state:
+stateTable=count(college, 'STABBR')
