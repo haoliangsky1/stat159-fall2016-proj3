5582333c2be135d250ed0b67402ca7db07e54f31
andrewshibata <ashibata@berkeley.edu>
2016-11-29 14:31:34 -0800

    ShinyApp: Added Fields of Study, Family Income, sorted States List

--- code/scripts/shinyApp-script.R
+++ code/scripts/shinyApp-script.R
@@ -1,5 +1,5 @@
 #setwd("~/Desktop/Fall_2016/Stat159/stat159-fall2016-proj3/")
-setwd("C:/Users/andre/Dropbox/College/Fall 16/Stat159/stat159-fall2016-proj3")
+#setwd("C:/Users/andre/Dropbox/College/Fall 16/Stat159/stat159-fall2016-proj3")
 library(ggplot2)
 library(stringr)
 library(shiny)
@@ -9,12 +9,14 @@ stateList = college$STABBR
 
 ui = fluidPage(
 	# Select a state
-	selectInput(inputId = 'state', label = 'Choose a State', choices = stateList),
+	selectInput(inputId = 'state', label = 'Choose a State', choices = sort(stateList)),
 	plotOutput('plot'),
 	# Select an intended major
-	selectInput(inputId = 'intendedMajor', label = 'Choose an intended major', choices = NA),
+	selectInput(inputId = 'intendedMajor', label = 'Choose an intended major', 
+	            choices = c('Computer Science','Literature','Mathematics','Engineering','Social Studies','Visual and Perfoming Arts','Business','History')),
 	# Select financial aid level
-	selectInput(inputId = 'financialAid', label = 'Choose a financial aid level', choices = NA)
+	selectInput(inputId = 'familyIncome', label = 'Choose family income level', 
+	            choices = c('$0-$30,000','$30,001-$48,000','$48,001-$75,000','$75,001-$110,000','$110,000+'))
 	# Select 
 	)
 
