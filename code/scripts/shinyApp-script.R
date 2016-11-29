b81557d0ca8f0bd4afd370736034aa31a633fe44
andrewshibata <ashibata@berkeley.edu>
2016-11-29 14:44:24 -0800

    ShinyApp: Added Ethnicity and SAT Scores

--- code/scripts/shinyApp-script.R
+++ code/scripts/shinyApp-script.R
@@ -11,13 +11,19 @@ ui = fluidPage(
 	# Select a state
 	selectInput(inputId = 'state', label = 'Choose a State', choices = sort(stateList)),
 	plotOutput('plot'),
+	# Select Race/Ethnicity
+	selectInput("ethnicity", label = 'Choose Ethnicity',
+	            choices = c('White', 'Black or African American', 'Hispanic', 'Asian', 'American Indian/Alaska Native', 'Native Hawaiian/Pacific Islander')),
 	# Select an intended major
 	selectInput(inputId = 'intendedMajor', label = 'Choose an intended major', 
 	            choices = c('Computer Science','Literature','Mathematics','Engineering','Social Studies','Visual and Perfoming Arts','Business','History')),
 	# Select financial aid level
 	selectInput(inputId = 'familyIncome', label = 'Choose family income level', 
-	            choices = c('$0-$30,000','$30,001-$48,000','$48,001-$75,000','$75,001-$110,000','$110,000+'))
-	# Select 
+	            choices = c('$0-$30,000','$30,001-$48,000','$48,001-$75,000','$75,001-$110,000','$110,000+')),
+	# Input SAT Scores
+	numericInput('SATMath', label = 'Input SAT Math Score', value=400),
+	numericInput('SATCriticalReading', label = 'Input SAT Critical Reading Score', value = 400),
+	numericInput('SATWriting', label = 'Input SAT Writing Score', value=400)
 	)
 
 server = function(input, output) {
