41640f79ecfec7b167832551d72dc31d8a1d8c67
andrewshibata <ashibata@berkeley.edu>
2016-11-29 14:21:33 -0800

    ShinyApp: Edited file pathway; added placeholder NA choices

--- code/scripts/shinyApp-script.R
+++ code/scripts/shinyApp-script.R
@@ -1,19 +1,20 @@
 #setwd("~/Desktop/Fall_2016/Stat159/stat159-fall2016-proj3/")
+setwd("C:/Users/andre/Dropbox/College/Fall 16/Stat159/stat159-fall2016-proj3")
 library(ggplot2)
 library(stringr)
 library(shiny)
 
-college = read.csv('data/mergedData.csv')
+college = read.csv('data/combinedData.csv')
 stateList = college$STABBR
 
 ui = fluidPage(
 	# Select a state
-	selectInput(inputId = 'state', label = 'Choose a State', choices = stateList)
-	plotOutput('plot')
+	selectInput(inputId = 'state', label = 'Choose a State', choices = stateList),
+	plotOutput('plot'),
 	# Select an intended major
-	selectInput(inputId = 'intendedMajor', label = 'Choose an intended major', choices = )
+	selectInput(inputId = 'intendedMajor', label = 'Choose an intended major', choices = NA),
 	# Select financial aid level
-	selectInput(inputId = 'financialAid', label = 'Choose a financial aid level', choices = )
+	selectInput(inputId = 'financialAid', label = 'Choose a financial aid level', choices = NA)
 	# Select 
 	)
 
