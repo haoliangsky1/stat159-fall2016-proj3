b8bc9ee8d826f87ac73e79fc9ca6fbfa24aadd3f
Liang Hao <haoliang302@gmail.com>
2016-11-30 20:13:29 -0800

    makeDummyDataFrame-script.R: initial commit to create dummy data frame

---
+++ code/scripts/makeDummyDataFrame-script.R
@@ -0,0 +1,5 @@
+# This file would take in the combinedData.csv dataset 
+# and select columns as dummy variables for modeling and shinyApp
+
+#setwd("~/Desktop/Fall_2016/Stat159/stat159-fall2016-proj3/")
+#college = read.csv('data/combinedData.csv')
