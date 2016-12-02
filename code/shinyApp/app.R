d26440a04376b5449a029053781da66e483bc806
Liang Hao <haoliang302@gmail.com>
2016-12-02 12:05:56 -0800

    app.R: changee UI layout

--- code/shinyApp/app.R
+++ code/shinyApp/app.R
@@ -7,8 +7,8 @@ library(grDevices)
 library(leaflet)
 library(DT)
 
-#source('../functions/plotGeo.R')
-source('../functions/makeTable.R')
+source('plotGeo.R')
+source('makeTable.R')
 
 dummyScore = read.csv('../../data/schoolRanking.csv')
 college = read.csv('../../data/combinedData.csv')
@@ -23,78 +23,95 @@ ui = fluidPage(
   # This is the title
   titlePanel('Custom College Match'),
   
-  # Main Panel
-  mainPanel(h1('Introduction'),
-            p('Custom Colllege Match finds the best schools for you to find the strongest schools for the best value.
-            Based on your information we will show you the top 5 schools for you based on factors such as net price, completion rate, and average earnings after graduation.'),
-            br(),
-            br(),
-            img(src = 'ggplot-schoolDistribution.png')
-            ),
-  
   # Sidebar
-  sidebarPanel('whatever you consider fit here'),
-  
-  
-  fluidRow(
-    column(3, h4('Basic Informaiton'), 
-           selectInput(inputId = 'state', label = 'Choose a State', choices = sort(stateList), selected = 'None'), br(),
-           # Select Race/Ethnicity
-           selectInput(inputId = "ethnicity", label = 'Choose Ethnicity',
-                       choices = c('White', 'Black or African American', 'Hispanic', 'Asian', 'American Indian/Alaska Native', 'Native Hawaiian/Pacific Islander'), 
-                       selected= 'None'),
-           br(),
-           # First Generation
-           radioButtons(inputId = 'firstGeneration', label = 'First Generation?', choices = c('Yes', 'No')),br(),
-           # Sex
-           radioButtons(inputId = 'sex', label = 'Select Sex', choices = c('Male', 'Female')),br()
+  sidebarLayout(
+    sidebarPanel(
+      h4('Map for all educational institutions in the recommendation system'),
+      plotOutput('mapNational')
+      
     ),
-    column(3, h4('Intended Major'),
-           # Select an intended major - Current list is placeholder
-           selectInput(inputId = 'intendedMajor', label = 'Choose an intended major', 
-                       choices = c('Computer Science','Literature','Mathematics','Engineering','Social Studies','Visual and Perfoming Arts','Business','History')),br()
+    
+    mainPanel(
+      h1('Introduction'),
+      helpText('Custom Colllege Match finds the best schools for you to find the strongest schools for the best value.
+               Based on your information we will show you the top 5 schools for you based on factors such as net price, completion rate, and average earnings after graduation.'),
+      fluidRow(
+        column(5, h4('Basic Informaiton'), 
+               selectInput(inputId = 'state', label = 'Choose a State', choices = sort(stateList), selected = 'None'),
+               # Select Race/Ethnicity
+               selectInput(inputId = "ethnicity", label = 'Choose Ethnicity',
+                           choices = c('White', 'Black or African American', 'Hispanic', 'Asian', 'American Indian/Alaska Native', 'Native Hawaiian/Pacific Islander'), 
+                           selected= 'None')
+      ),
+      column(5, h4('Financial Aid'),
+             # Select financial aid level - Data has net cost after financial aid based on family income
+             selectInput(inputId = 'familyIncome', label = 'Choose family income level', 
+                         choices = c('$0-$30,000','$30,001-$48,000','$48,001-$75,000','$75,001-$110,000','$110,000+')),
+             br(),
+             h4('Intended Major'),
+             # Select an intended major - Current list is placeholder
+             selectInput(inputId = 'intendedMajor', label = 'Choose an intended major', 
+                         choices = c('Computer Science','Literature','Mathematics','Engineering','Social Studies','Visual and Perfoming Arts','Business','History'))
+        
+      )
     ),
-    column(3, h4('Financial Aid'),
-           # Select financial aid level - Data has net cost after financial aid based on family income
-           selectInput(inputId = 'familyIncome', label = 'Choose family income level', 
-                       choices = c('$0-$30,000','$30,001-$48,000','$48,001-$75,000','$75,001-$110,000','$110,000+')),br()
+    fluidRow(
+      column(5, h4('Standardized Test'),
+             # Input SAT Scores - Data has percentiles of SAT/ACT scores for students
+             numericInput(inputId = 'SATMath', label = 'Input SAT Math Score', value=NA),
+             numericInput(inputId = 'SATCriticalReading', label = 'Input SAT Critical Reading Score', value = NA),
+             #numericInput(inputId = 'SATWriting', label = 'Input SAT Writing Score', value=NA),
+             # Input ACT Scores
+             numericInput(inputId = 'ACTEnglish', label = 'Input ACT English Score', value=NA),
+             numericInput(inputId = 'ACTMath', label = 'Input ACT Math Score', value=NA)
+             #numericInput(inputId = 'ACTWriting', label = 'Input ACT Writing Score', value=NA)
+      ),
+      column(5,  # First Generation
+             br(),
+             radioButtons(inputId = 'firstGeneration', label = 'First Generation?', choices = c('Yes', 'No')),
+             br(),
+             # Sex
+             radioButtons(inputId = 'sex', label = 'Select Sex', choices = c('Male', 'Female'))
+      )
     ),
-    column(3, h4('Standardized Test'),
-           # Input SAT Scores - Data has percentiles of SAT/ACT scores for students
-           numericInput(inputId = 'SATMath', label = 'Input SAT Math Score', value=NA),
-           numericInput(inputId = 'SATCriticalReading', label = 'Input SAT Critical Reading Score', value = NA),
-           #numericInput(inputId = 'SATWriting', label = 'Input SAT Writing Score', value=NA),
-           # Input ACT Scores
-           numericInput(inputId = 'ACTEnglish', label = 'Input ACT English Score', value=NA),
-           numericInput(inputId = 'ACTMath', label = 'Input ACT Math Score', value=NA)
-           #numericInput(inputId = 'ACTWriting', label = 'Input ACT Writing Score', value=NA)
+    sliderInput('range',
+                label = 'I dont know yet: ',
+                min = 0, max =100, value = c(0, 100)),
+    
+    actionButton("action", label= 'Go'),
+    br()
     )
   ),
-  sliderInput('I dont know yet',
-              label = 'I dont know yet: ',
-              min = 0, max =100, value = c(0, 100)),
-  
-  actionButton("action", label= 'Go'),
-  br(),
-  fluidRow(
-    column(2, h1('Lets put some text here'),
-           textOutput('text1')
-           ),
-    column(3, h1('Now is the table'),
-           DT::dataTableOutput('table')
-           )
-  ),
-  br()
+  # Main Panel
+  mainPanel(h1('This is the panel for responses'),
+            p(''),
+            br(),
+            textOutput('text1'),
+            textOutput('text2'),
+            h4('lets first show a map:'),
+            plotOutput('map'),
+            br(),
+            h4('then we may layout the table:'),
+            DT::dataTableOutput('table')
+  )
   )
 # Define server logic required to draw a histogram
 server = function(input, output) {
   output$text1 = renderText({
     paste('Welcome, student from', input$state)
   })
+  output$text2 = renderText({
+    paste('So we see that you selected a range from,',
+          input$range[1], ' to ', input$range[2])
+  })
   
-  output$plot = renderPlot({
+  output$mapNational =renderPlot({
+    plotGeo('all', dummyScore)
+  })
+  
+  output$map = renderPlot({
     stateName = input$state
-    #plotGeo(stateName, dummyScore)
+    plotGeo(stateName, dummyScore)
     #tags$div(img(src = 'choiceOfState.png'))
     
   })
