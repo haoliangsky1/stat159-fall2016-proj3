8802cf4453222c4a9d092e9bb399da91a641de67
Liang Hao <haoliang302@gmail.com>
2016-12-01 21:04:31 -0800

    shinyApp-script.R: modify to call makeTable function

--- code/scripts/shinyApp-script.R
+++ code/scripts/shinyApp-script.R
@@ -7,6 +7,9 @@ library(maps)
 library(ggplot2)
 library(grDevices)
 library(leaflet)
+library(DT)
+
+#source('../functions/plotGeo.R')
 
 dummyScore = read.csv('../../data/schoolRanking.csv')
 
@@ -14,11 +17,6 @@ dummyScore = read.csv('../../data/schoolRanking.csv')
 stateList = college$STABBR
 all_states = map_data('state')
 all_states[]
-
-
-
-
-
 
 
 
@@ -39,12 +37,12 @@ shinyUI = fluidPage(
     column(3, h4('Basic Informaiton'), 
            selectInput(inputId = 'state', label = 'Choose a State', choices = sort(stateList)), br(),
            # Select Race/Ethnicity
-           selectInput("ethnicity", label = 'Choose Ethnicity',
+           selectInput(inputID = "ethnicity", label = 'Choose Ethnicity',
                        choices = c('None','White', 'Black or African American', 'Hispanic', 'Asian', 'American Indian/Alaska Native', 'Native Hawaiian/Pacific Islander')),br(),
            # First Generation
-           radioButtons('firstGeneration', label = 'First Generation?', choices = c('Yes', 'No')),br(),
+           radioButtons(inputID = 'firstGeneration', label = 'First Generation?', choices = c('Yes', 'No')),br(),
            # Sex
-           radioButtons('sex', label = 'Select Sex', choices = c('Male', 'Female')),br()
+           radioButtons(inputID = 'sex', label = 'Select Sex', choices = c('Male', 'Female')),br()
            ),
     column(3, h4('Intended Major'),
            # Select an intended major - Current list is placeholder
@@ -58,36 +56,45 @@ shinyUI = fluidPage(
            ),
     column(3, h4('Standardized Test'),
            # Input SAT Scores - Data has percentiles of SAT/ACT scores for students
-           numericInput('SATMath', label = 'Input SAT Math Score', value=NA),
-           numericInput('SATCriticalReading', label = 'Input SAT Critical Reading Score', value = NA),
-           numericInput('SATWriting', label = 'Input SAT Writing Score', value=NA),
+           numericInput(inputId = 'SATMath', label = 'Input SAT Math Score', value=NA),
+           numericInput(inputId = 'SATCriticalReading', label = 'Input SAT Critical Reading Score', value = NA),
+           #numericInput(inputId = 'SATWriting', label = 'Input SAT Writing Score', value=NA),
            # Input ACT Scores
-           numericInput('ACTEnglish', label = 'Input ACT English Score', value=NA),
-           numericInput('ACTMath', label = 'Input ACT Math Score', value=NA),
-           numericInput('ACTWriting', label = 'Input ACT Writing Score', value=NA)
+           numericInput(inputId = 'ACTEnglish', label = 'Input ACT English Score', value=NA),
+           numericInput(inputId = 'ACTMath', label = 'Input ACT Math Score', value=NA),
+           #numericInput(inputId = 'ACTWriting', label = 'Input ACT Writing Score', value=NA)
            )
     ),
   actionButton("action", label= 'Go'),
+  br(),
+  fluidRow(
+    DT::dataTableOutput('table')
+  ),
   br()
   
 #  plot('plot')
   #plotOutput('plot'),
 	)
-p = ggplot()
-p = p + geom_polygon(data = all_states[all_states$region == 'california',], 
-                     aes(x=long, y= lat, group = group), color = 'white')
-temp = dummyScore[dummyScore$STABBR == stateName, ]
-temp = temp[order(-temp$Score), ]
-p = p + geom_point(data = temp[1:20,], aes(x = LONGITUDE, y = LATITUDE, size = Score),
-                   color = 'red') + scale_size(name = 'Score')
-p
 
 shinyServer = function(input, output) {
-  stateName =input$state
+  stateName = input$state
+  ethnicity = input$ethnicity
+  income = input$familyIncome
+  SATMath = input$SATMath
+  SATCR = input$SATCriticalReading
+  ACTEng = input$ACTEnglish
+  ACTMath = input$ACTMath
+  
   output$plot = renderPlot({
     stateName = input$state
-    plotGeo()
+    #plotGeo(stateName, dummyScore)
+    #tags$div(img(src = 'choiceOfState.png'))
+    
   })
+  output$table = DT::renderDataTable(DT::datatable({
+    table = makeTable(dummyScore, stateName, ethnicity, income, SATMath, SATCR, ACTEng, ACTMath)
+
+  }))
 
 
   
@@ -97,17 +104,20 @@ shinyServer = function(input, output) {
 	#	})
 }
 
-plotGeo = function(){
-  map = get_map(location = 'North America', zoom =4)
-  mapPoints = ggmap(map) +
-    geom_point(data = dummyScore, aes(x = LONGITUDE, y = LATITUDE, size= Score), alpha = .3,
-               color = 'red') + scale_size(name = 'Score') 
-  
-  mapPoints
-}
 
 
 shinyApp(ui = shinyUI, server = shinyServer)
 
 
+
+
+
+p = ggplot()
+p = p + geom_polygon(data = all_states[all_states$region == 'california',], 
+                     aes(x=long, y= lat, group = group), color = 'white')
+temp = dummyScore[dummyScore$STABBR == stateName, ]
+temp = temp[order(-temp$Score), ]
+p = p + geom_point(data = temp[1:20,], aes(x = LONGITUDE, y = LATITUDE, size = Score),
+                   color = 'red') + scale_size(name = 'Score')
+p
 
