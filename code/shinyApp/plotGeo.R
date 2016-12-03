046afcc401cab18fb5a987832ae747e26579eb23
Liang Hao <haoliang302@gmail.com>
2016-12-03 13:06:28 -0800

    plotGeo.R: making state specific graphs

--- code/shinyApp/plotGeo.R
+++ code/shinyApp/plotGeo.R
@@ -5,11 +5,11 @@ plotGeo = function(stateName, df){
 		map = get_map(location = 'North America', zoom = 4)
 		mapPoints = ggmap(map) + geom_point(data=df, aes(x = LONGITUDE, y = LATITUDE, size= Score), alpha= .3, color = 'red') + scale_size('Score')
 		} else {
-			temp = df[df$STABBR == stateName, ]
-            temp = temp[order(-temp$Score), ]
-            temp = temp[1:20, ]
-            map = get_map(location = 'North America', zoom = 4)
-            mapPoints = ggmap(map) +
+      temp = df[df$STABBR == stateName, ]
+      temp = temp[order(-temp$Score), ]
+      temp = temp[1:20, ]
+      map = get_map(location = stateName, zoom = 6)
+      mapPoints = ggmap(map) +
             geom_point(data = temp, aes(x = LONGITUDE, y = LATITUDE, size= Score), alpha = .3,
             	color = 'red') + scale_size(name = 'Score') 
   # png('www/choiceOfState.png')
@@ -19,3 +19,4 @@ plotGeo = function(stateName, df){
 		mapPoints
   
 }
+
