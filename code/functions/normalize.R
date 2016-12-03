7a3ac9e9610ea612fe0511c9dc28747c358690da
Liang Hao <haoliang302@gmail.com>
2016-12-03 13:23:13 -0800

    normalize.R: adding unit tests

--- code/functions/normalize.R
+++ code/functions/normalize.R
@@ -1,4 +1,4 @@
 # This function normalize the given vector with the input scale
-normalize = function(x, from, to) {
-	(x - min(x)) / max(x - min(x)) * (to - from) + from
+normalize = function(x, from=0, to=100) {
+	(x - min(x, na.rm =T)) / max(x - min(x, na.rm=T), na.rm=T) * (to - from) + from
 }
--- code/tests/test-selfDefineFunctions-script.R
+++ code/tests/test-selfDefineFunctions-script.R
@@ -2,5 +2,30 @@
 # please feel free to add in any for research purpose or convenience.
 # Here is the file to set up any tests for the self-defined functions.
 library(testthat)
+print(getwd())
 context('Test for selfDefineFunctions.R functions')
+
+source('../functions/normalize.R')
+context('Test for normailze function')
+
+test_that('Test for proper functions', {
+  x = c(0:10)
+  ans = normalize(x, 0, 100)
+  expect_equal(ans, seq.int(0, 100, 10))
+  expect_length(ans, length(x))
+  expect_type(ans, 'double')
+})
+
+test_that('Test for NA', {
+  x = c(0:10, NA, NA)
+  ans = normalize(x, 0, 100)
+  expect_equal(ans, c(seq.int(0, 100, 10), NA, NA))
+  expect_length(ans, length(x))
+  expect_type(ans, 'double')
+})
+
+test_that('Test for incompatible type', {
+  x = letters[1:5]
+  expect_that(normalize(x), throws_error())
+})
 
