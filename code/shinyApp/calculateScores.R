04da0c160009d334b70bcb4efd1d0cb27d86849b
Liang Hao <haoliang302@gmail.com>
2016-12-04 15:26:52 -0800

    calculateScores.R: incorporate weights for intended major

--- code/shinyApp/calculateScores.R
+++ code/shinyApp/calculateScores.R
@@ -1,32 +1,34 @@
 # This function calculates the Scores given the input of the user
 library(microbenchmark)
 
-calculateScores = function(dummy, income='$0-$30,000', firstGen=TRUE, useDefaultWeights=FALSE) {
+calculateScores = function(dummy, income='$0-$30,000', firstGen=TRUE, major='None', useDefaultWeights=FALSE) {
 	if (useDefaultWeights == TRUE) {
-		weight = c(-0.05, -0.05, -0.05, 0.1, 0.2, 0.1, 0.1, 0.5, 0.5, 0.5, 0.1, -0.1, 0.2)
+		# weight = c(-0.05, -0.05, -0.05, 0.1, 0.2, 0.1, 0.1, 0.5, 0.5, 0.5, 0.1, -0.1, 0.2)
+		weight = c(-0.05, -0.05, -0.05, 0.1, 0.2, 0.1, 0.1, 0.5, 0.5, 0.5, 0.1, -0.1, 0.2, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1)
 	} else {
-	  weight = rep(0, 13)
+	  # weight = rep(0, 13)
+	  weight = rep(0, 22)
 	  if (income == '$0-$30,000') {
-	    weight[1] = -0.5
+	    weight[1] = -0.3
 	    } else if (income == '$30,001-$48,000') {
-	      weight[2] = -0.5
+	      weight[2] = -0.3
 	      } else if (income == '$48,001-$75,000') {
-	        weight[3] = -0.5
+	        weight[3] = -0.3
 	        } else if (income == '$75,001-$110,000') {
-	          weight[4] = 0.5
+	          weight[4] = 0.4
 	          } else if (income == '$110,000+') {
-	            weight[5] = 0.5
+	            weight[5] = 0.4
 	            }
 	  # 3 year repayment rate
 	  weight[6] = 0.1
 	  # 5 year repyament rate
 	  weight[7] = 0.1
 	  # 4 year completion rate
-	  weight[8] = 0.5
+	  weight[8] = 0.8
 	  # 10 year mean earnings
-	  weight[9] = 0.5
+	  weight[9] = 0.8
 	  # 10 year median earnings
-	  weight[10] = 0.5
+	  weight[10] = 0.8
 	  # Pell Grant:
 	  if (income == '$0-$30,000') {
 	    weight[11] = 0.5
@@ -37,6 +39,29 @@ calculateScores = function(dummy, income='$0-$30,000', firstGen=TRUE, useDefault
 	  if (firstGen == TRUE) {
 	    weight[13] = 0.2
 	    }
+
+	  #Intended Major
+	  if (major == 'None') {
+	    weight[14:22] = 0.1
+	    } else if (major == 'Computer Science') {
+	      weight[14] = 0.3
+	      } else if (major == 'Literature') {
+	        weight[15] = 0.3
+	        } else if (major == 'Mathematics/Statistics'){
+	          weight[16] = 0.3
+	          } else if (major == 'Engineering') {
+	            weight[17] = 0.3
+	            } else if (major == 'Social Studies/Humanities') {
+	              weight[18] = 0.3
+	              } else if (major == 'Visual and Performing Arts') {
+	                weight[19] = 0.3
+	                } else if (major == 'Business') {
+	                  weight[20] = 0.3
+	                  } else if (major == 'History') {
+	                    weight[21] = 0.3
+	                    } else if (major == 'Life Science/Health') {
+	                      weight[22] = 0.3
+	                      }
 	}
 	# Centralization
 	df = centralization(dummy)
