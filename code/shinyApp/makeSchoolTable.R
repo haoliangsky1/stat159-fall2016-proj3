74e7a6e91db09d0f136d5d71f0097e1baa1a02bf
Liang Hao <haoliang302@gmail.com>
2016-12-04 15:51:27 -0800

    makeSchoolTable.R: adding getMajor function for percentage

--- code/shinyApp/makeSchoolTable.R
+++ code/shinyApp/makeSchoolTable.R
@@ -1,20 +1,23 @@
 # This file contains a helper function for the shinyApp.
 # This function takes in all the necessary information from the user and returns a table
 
-makeSchoolTable = function(df, rawDF, schoolID, stateName, ethnicity, income, SATMath, SATCR, ACTEng, ACTMath) {
+makeSchoolTable = function(df, rawDF, schoolID, stateName, ethnicity='None', income='$0-$30,000', major='None', SATMath=0, SATCR=0, ACTEng=0, ACTMath=0) {
 	# We want to deliver a table with essential information for the top 10 choices in state or even nationally
-	result = data.frame(0,0,0)
-	colnames(result) = c('Name', 'NetPrice', 'PercentageEthnicity')
+	result = data.frame(0,0,0,0)
+	colnames(result) = c('Name', 'NetPrice', 'PercentageEthnicity', 'PercentageMajor')
 	if (is.na(schoolID)) {
 		return(result)
 	} else {
 	  schoolName = as.vector(df$INSTNM[df$UNITID == schoolID])
 	  schoolScore = df$Score[df$UNITID == schoolID]
-	  result['Name'] = schoolName
+	  # Name
+	  result$Name = schoolName
 	  # Net Price
 	  result$NetPrice = getNetPrice(df, schoolID, income)
 	  # Ethnicity
 	  result$PercentageEthnicity = getPercentageOfEthnicity(rawDF, schoolID, ethnicity)
+	  # Major
+	  result$PercentageMajor = getPercentageOfMajor(df, schoolID, major)
 	  return(result)
 	}
 }
@@ -55,6 +58,33 @@ getPercentageOfEthnicity = function(rawDF, schoolID, ethnicity) {
 		percentageOfEthnicity = temp$UGDS_NHPI
 	}
 	return(percentageOfEthnicity)
+}
+
+getPercentageOfMajor = function(df, schoolID, major) {
+	temp = df[df$UNITID == schoolID, ]
+	choices = c('None','Computer Science','Literature','Mathematics/Statistics','Engineering','Social Studies/Humanities',
+                                     'Visual and Perfoming Arts','Business','History', 'Life Science/Health')
+	PercentageOfMajor = NA
+	if (major == 'Computer Science') {
+		PercentageOfMajor = temp$DegreesComputerScience
+	} else if (major == 'Literature') {
+	    PercentageOfMajor = temp$DegreesLiterature
+	} else if (major == 'Mathematics/Statistics'){
+	    PercentageOfMajor = temp$DegreesMathematics
+	} else if (major == 'Engineering') {
+	    PercentageOfMajor = temp$DegreesEngineering
+	} else if (major == 'Social Studies/Humanities') {
+	    PercentageOfMajor = temp$DegreesSocialStudies
+    } else if (major == 'Visual and Performing Arts') {
+        PercentageOfMajor = temp$DegreesVisualPerformingArts
+    } else if (major == 'Business') {
+        PercentageOfMajor = temp$DegreesBusiness
+    } else if (major == 'History') {
+        PercentageOfMajor = temp$DegreesHistory
+    } else if (major == 'Life Science/Health') {
+        PercentageOfMajor = temp$DegreesLifeScienceHealth
+    }
+	return(PercentageOfMajor)
 }
 
 
