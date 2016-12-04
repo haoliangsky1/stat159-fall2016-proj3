7d1656eb48e06f310537fc43806062be5535b193
Liang Hao <haoliang302@gmail.com>
2016-12-03 16:35:24 -0800

    makeRankingTable.R

--- code/shinyApp/makeRankingTable.R
+++ code/shinyApp/makeRankingTable.R
@@ -1,8 +1,7 @@
 # This file contains a helper function for the shinyApp.
 # This function returns schools with rankings
 
-
-makeRankingTable = function(df, stateName) {
+makeRankingTable = function(df, stateName, income, firstGen) {
 	if (stateName == 'None') {
 		state = df[, c('UNITID', 'INSTNM', 'STABBR','Score')]
 	} else {
@@ -11,11 +10,13 @@ makeRankingTable = function(df, stateName) {
 	schoolID = as.vector(state$UNITID)
 	schoolName = as.vector(state$INSTNM)
 	schoolScore = as.numeric(state$Score)
-	result = state[,c('INSTNM', 'STABBR','Score')]
+	result = state[,c('UNITID','INSTNM', 'STABBR','Score')]
 	result = result[order(-result$Score), ]
 	output = result[, ]
 	output['Ranking'] = c(1:nrow(output))
-	colnames(output) = c('Name', 'State', 'Score', 'Ranking')
+	colnames(output) = c('ID','Name', 'State', 'Score', 'Ranking')
 	return(output)
 }
+
+
 
